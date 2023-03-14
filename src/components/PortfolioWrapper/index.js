993a0a7979e3effc903c1bc8721838c0cda8f559
vcdsc <117476319+vcdsc@users.noreply.github.com>
2023-03-14 20:57:27 +0000

    Updates

--- src/components/PortfolioWrapper/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./style.css";
-
-function PortfolioWrapper(props) {
-  return <main className="portfolio-wrapper">{props.children}</main>;
-}
-
-export default PortfolioWrapper;
